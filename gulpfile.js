a026f72d908885615e628bf16fbee348df90c7be
Chirag Vashist <chiragvashist007@gmail.com>
2019-02-22 23:37:21 +0530

    Init Commit

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp'),
+  del = require('del'),
+  rename = require('gulp-rename'),
+  runSequence = require('run-sequence'),
+  htmlmin = require('gulp-htmlmin'),
+  minify = require('gulp-minify'),
+  csso = require('gulp-csso'),
+  eslint = require('gulp-eslint'),
+  gulpIf = require('gulp-if')
+  ;
+
+gulp.task('default', function() {
+  console.log('Please use the following gulp tasks: clean, build');
+});
+
+gulp.task('clean', function() {
+  return del('./dist', {
+    force: true
+  });
+});
+
+gulp.task('pages', function() {
+  return gulp.src(['./lib/*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./lib/styles/*.css')
+    .pipe(csso())
+    .pipe(gulp.dest('./dist/styles'))
+});
+
+gulp.task('esLint',()=>{
+  gulp.src('./lib/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .on('error', function(err) {
+      console.log("Run 'gulp-fix' in terminal to fix these errors"); 
+      process.exit()
+    });
+})
+
+function isFixed(file) {
+  return file.eslint != null && file.eslint.fixed;
+}
+
+gulp.task('fix', function () {
+  return gulp.src('./lib/*.js')
+    .pipe(eslint({fix:true}))
+    .pipe(eslint.format())
+    .pipe(gulpIf(isFixed, gulp.dest('lib/')))
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('minify', function () {
+  return gulp.src('./lib/*.js')
+    .pipe(minify({
+      ext: {
+        min: '.js'
+      },
+      noSource: true,
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-dist', function () {
+  gulp.src('./lib/_locales/**/*').pipe(gulp.dest('./dist/_locales/'));
+  gulp.src('./lib/assets/**/*').pipe(gulp.dest('./dist/assets/'));
+  return gulp.src('./lib/manifest.json').pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build', function() {
+    runSequence('clean', 'pages', 'styles', 'esLint', 'minify', 'copy-dist');
+});
