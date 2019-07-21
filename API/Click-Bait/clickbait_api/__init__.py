4003fb3babf3a2dfcc7ae950aef4c67d5f75d07f
Utsav Shukla <us241077@gmail.com>
2019-07-21 06:32:41 +0530

    added swagger UI documentation for both APIs

--- API/Click-Bait/clickbait_api/__init__.py
+++ API/Click-Bait/clickbait_api/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask_cors import CORS, cross_origin
 import logging
 from logging.handlers import RotatingFileHandler
+from flask_swagger_ui import get_swaggerui_blueprint
 
 # initialize the log handler
 logHandler = RotatingFileHandler('info.log', maxBytes=1000, backupCount=1)
@@ -11,14 +12,29 @@ logHandler = RotatingFileHandler('info.log', maxBytes=1000, backupCount=1)
 logHandler.setLevel(logging.INFO)
 
 app = Flask(__name__)
+###logger specific ###
 app.logger.setLevel(logging.INFO)
 app.logger.addHandler(logHandler) 	
 app.config.from_object('config')
 app.logger.setLevel(logging.INFO)
+app.logger.addHandler(logHandler)
+ 
 
-app.logger.addHandler(logHandler) 
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Social Street Smart - Clickbait"
+    }
+)
 
 
+
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 cors = CORS(app)
 from clickbait_api import routes
 
