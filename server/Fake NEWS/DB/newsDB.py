2b21e3229146a43035d4bd7c25f0c04a7803f34d
ad_6398 <kumaramardipsingh@gmail.com>
2019-08-06 02:47:41 +0530

    add db func of all news

--- server/Fake NEWS/DB/newsDB.py
+++ server/Fake NEWS/DB/newsDB.py
@@ -22,3 +22,30 @@ class authenticNews(db.Model):
             'content': self.content
         }
 
+class allnewsDB(db.Model):
+    __tablename__='allNewsDBtable'
+    link= db.Column(db.String(), primary_key=True)
+    source= db.Column(db.String())
+    dateTime= db.Column(db.String()) # make this in time 
+    content= db.Column(db.String())
+    def __init__(self, link, source,dateTime, content):
+        self.link = link
+        self.source = source
+        self.dateTime= dateTime
+        self.content= content
+
+    def __repr__(self):
+        return '<allNewsDB %r>' % self.content
+
+    def serialize(self):
+        return{
+            'link': self.link,
+            'source': self.source,req= request.json
+    reqURL= req['url']
+    newsPost= getNews(reqURL)
+    r=jsonify({'result':newsPost})
+    r.headers.add('Access-Control-Allow-Origin', '*')           #to solve cross origin request problem, modify this in future
+    return r
+            'dateTime': self.dateTime,
+            'content': self.content
+        }
