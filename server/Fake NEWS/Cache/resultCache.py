b6c059fcee517b1c37d2826699057aee5b391220
ad_6398 <kumaramardipsingh@gmail.com>
2019-08-14 16:57:14 +0530

    add cache for twitter results

--- server/Fake NEWS/Cache/resultCache.py
+++ server/Fake NEWS/Cache/resultCache.py
@@ -1,5 +1,7 @@
 from application import db
 
+#Cache DB for facebook
+
 class fbCache(db.Model):
     __tablename__= 'fbCacheTable'
     fbID= db.Column(db.String(), primary_key=True)
@@ -22,4 +24,33 @@ class fbCache(db.Model):
             'dateTime': self.dateTime,
             'result': self.result
         }
+
+#Cache DB for Twitter post's Result
+
+class tweetCache(db.Model):
+    __tablename__='tweetCacheTable'
+    tweetID= db.Column(db.String(),primary_key= True)
+    sourceHandle= db.Column(db.String())
+    dateTime= db.Column(db.String())
+    result= db.Column(db.String())
+    def __init__(self,tweetID,sourceHandle,dateTime, result):
+        self.tweetID = tweetID
+        self.sourceHandle = sourceHandle
+        self.dateTime = dateTime
+        self.result = result
+    
+    def __repr__(self):
+        return '<tweetCache %r>' % self.tweetID
+
+    def serialize(self):
+        return{
+            'tweetID': self.tweetID,
+            'sourceHandle': self.sourceHandle,
+            'dateTime': self.dateTime,
+            'result': self.result
+        }
+
+#Cache DB for news websites
+
+
 
