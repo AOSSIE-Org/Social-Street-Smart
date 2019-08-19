4cfab35e1370654afeb9555c18d066fd5674cb9b
ad_6398 <kumaramardipsingh@gmail.com>
2019-08-20 03:26:50 +0530

    add scraper for The Hindu

--- server/Fake NEWS/News_Scrapper/authNS.py
+++ server/Fake NEWS/News_Scrapper/authNS.py
@@ -9,4 +9,17 @@ options.add_argument('--headless')
 from News_Scrapper.newsWebScrap import getNews
 import time
 
+def scrap_Thindu(hindu_url="https://www.thehindu.com/news/international/"):
+    print("schduled scraping from the hindu")
+    d2= webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
+    d2.get(hindu_url)
+    print("loading hindu loaded")
+    hindu_page_source= d2.page_source
+    hsoup = BeautifulSoup(hindu_page_source, 'lxml')
+    hfeeds= hsoup.find_all('div', class_= 'Other-StoryCard')
+    h_url_li= []
+    for x in hfeeds:
+        h_url_li.append(x.find("a")["href"])
+    return h_url_li
+
 
