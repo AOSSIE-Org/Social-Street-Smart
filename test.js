c966a27f6cfc5fadd443934def0be2f7bedb1ecd
Pranav Goyanka <pgoyanka@gmail.com>
2020-06-08 18:38:58 +0530

    Made extension id dynamically allocation and other bug fixes

--- test.js
+++ test.js
@@ -4,76 +4,24 @@ const extensionPath = './lib'
 let browser = null;
 let testingPage = null;
 let optionsPage = null;
-let optionsUrl = "chrome-extension://kgofljnbemgcgneaigebncpcmancipeg/views/settings.html";
-let className = "";
-
-
-// test("reddit", async () => {
-//     const url = 'https://www.reddit.com/r/testingground4bots/comments/fagpst/you_wont_believe_what_happened_next/'
-//     const page = await browser.newPage();
-//     await page.goto(url, {waitUntil: 'load', timeout: 0});
-//     expect(5).toBeGreaterThan(0);
-//     page.close()
-// }, 70000);
+// let optionsUrl = "chrome-extension://kgofljnbemgcgneaigebncpcmancipeg/views/settings.html";
+let className = "CB_twitter";
+let extensionID = "";
 
 describe('Extension Test', function(){
+    
     this.timeout(100000);
     before(async function() {
         await boot();
-
-        
     });
 
     // className = ["CB_twitter", "CB_reddit"];
-    className = "CB_twitter";
+    // className = "CB_twitter";
     optionsTest()
-
-    describe('First test', async function(){
-        it('IDK', async function(){
-            // let newUrl = "https://www.reddit.com/r/testingground4bots/comments/fagpst/you_wont_believe_what_happened_next/"
-            let newUrl = "https://twitter.com/BuzzFeed/status/1269750893009870848"
-            // await testingPage.waitFor(2000)
-            await testingPage.goto(newUrl)
-            await testingPage.waitFor(3000);
-            const CB = await testingPage.$eval('.SSS', el => el.textContent)
-            console.log(CB)
-            // let t = true;
-            let t = CB.includes("Clickbait")
-            // assert.equal(CB, "Clickbait")
-            assert.equal(t, true);
-        })
-    });
-
-    className = "CB_twitter";
-    optionsTest()
-
-    // describe ('Options' , async function(){
-    //     it('IDK', async function(){
-    //         await SSSoptions();
-    //         await optionsPage.close()
-    //     })
-    // });
-
-    
-    // describe('Second test', async function(){
-    //     it('IDK', async function(){
-    //         let newUrl = "https://www.reddit.com/r/Guitar/comments/gypr3e/play_burnin_for_you_blue_%C3%B6yster_cult_all_guitars/"
-    //         await testingPage.goto(newUrl)
-    //         const subName = await testingPage.$eval('._19bCWnxeTjqzBElWZfIlJb', el => el.textContent)
-    //         console.log(subName)
-    //         assert.equal(subName,'r/Guitar')
-    //     })
+    // test1();
+    // test1();
 
 
-    // });
-
-    // describe ('Options' , async function(){
-    //     it('IDK', async function(){
-    //         await SSSoptions();
-    //     })
-    // });
-
-    // optionsTest()
 
     after(async function(){
         await browser.close()
@@ -95,40 +43,88 @@ async function boot() {
 
     // Define the Chorome tabs/pages
 
-    // optionsPage = await browser.newPage();
     testingPage = await browser.newPage();
-    // await page.waitFor(7000);
-    // await optionsPage.goto("chrome-extension://kgofljnbemgcgneaigebncpcmancipeg/views/settings.html")
 
-    // const url = 'https://www.reddit.com/r/testingground4bots/comments/fagpst/you_wont_believe_what_happened_next/'
-    // await testingPage.goto(url)
+    const dummyPage = await browser.newPage();
+    // await dummyPage.waitFor(10000); // arbitrary wait time.
+  
+    const targets = await browser.targets();
+    const extensionTarget = targets.find(({ _targetInfo }) => {
+      return _targetInfo.title === 'Social Street Smart';
+    });
+  
+    const extensionUrl = extensionTarget._targetInfo.url || '';
+    [,, extensionID] = extensionUrl.split('/');
+    dummyPage.close()
+    // optionsPage = await browser.newPage();
+    // await optionsPage.goto("chrome-extension://"+ extensionID +"/views/settings.html")
+    // const toggle = await optionsPage.$('.' + className, el => el.outerHTML);
+    // await toggle.click();
+    // const saveSettings = await optionsPage.$('#save_settings', el => el.outerHTML);
+    // await optionsPage.waitFor(5000)
+    // await saveSettings.click();
+
+
 }
 
 
 // Opens the options page for SSS
-async function SSSoptions(){
+// async function SSSoptions(){
 
-    optionsPage = await browser.newPage();
-    // await page.waitFor(7000);
-    await optionsPage.goto("chrome-extension://kgofljnbemgcgneaigebncpcmancipeg/views/settings.html")
-    // className.forEach( (opt) => {
-    //     const toggle = await optionsPage.$('.' + opt, el => el.outerHTML);
-    // // await page.waitFor(3000);
-    //     await toggle.click();
-    // })
-    const toggle = await optionsPage.$('.' + className, el => el.outerHTML);
-    // await page.waitFor(3000);
-    await toggle.click();
-    const saveSettings = await optionsPage.$('#save_settings', el => el.outerHTML);
-    await saveSettings.click();
-    // await page.waitFor(5000);
+//     optionsPage = await browser.newPage();
+//     await optionsPage.goto("chrome-extension://"+ extensionID +"/views/settings.html")
+//     const toggle = await optionsPage.$('.' + className, el => el.outerHTML);
+//     await toggle.click();
+//     const saveSettings = await optionsPage.$('#save_settings', el => el.outerHTML);
+//     await saveSettings.click();
 
-}
+// }
 
 async function optionsTest(){
     describe ('Options' , async function(){
         it('IDK', async function(){
-            await SSSoptions();
+            // await SSSoptions();
+            optionsPage = await browser.newPage();
+            await optionsPage.goto("chrome-extension://"+ extensionID +"/views/settings.html")
+            const toggle = await optionsPage.$('.' + className, el => el.outerHTML);
+            await toggle.click();
+            const saveSettings = await optionsPage.$('#save_settings', el => el.outerHTML);
+            await optionsPage.waitFor(2000)
+            await saveSettings.click();
+
+            let newUrl = "https://twitter.com/BuzzFeed/status/1269750893009870848"
+            // await testingPage.waitFor(2000)
+            await testingPage.goto(newUrl)
+            await testingPage.waitFor(3000);
+
+            const CB = await testingPage.$eval('.SSS', el => el.textContent)
+            console.log(CB)
+            let t = CB.includes("Clickbait")
+            assert.equal(t, true);
+
         })
     });
+
+    // test1()
+}
+
+async function test1(){
+    describe('First test', async function(){
+        it('IDK', async function(){
+            // let newUrl = "https://www.reddit.com/r/testingground4bots/comments/fagpst/you_wont_believe_what_happened_next/"
+            let newUrl = "https://twitter.com/BuzzFeed/status/1269750893009870848"
+            // await testingPage.waitFor(2000)
+            await testingPage.goto(newUrl)
+            await testingPage.waitFor(3000);
+
+            const CB = await testingPage.$eval('.SSS', el => el.textContent)
+            console.log(CB)
+            // let t = true;
+            let t = CB.includes("Clickbait")
+            // assert.equal(CB, "Clickbait")
+            assert.equal(t, true);
+        })
+    });
+
+    return true;
 }
