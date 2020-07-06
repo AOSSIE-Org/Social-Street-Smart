014569f6faffa67b124320943c0d1546265bff48
Pranav Goyanka <pgoyanka@gmail.com>
2020-07-06 16:51:24 +0530

    remove gitignore files

--- server/News_Origin/.serverless/newsOrigin-aossie/app.py
+++
@@ -1,5 +0,0 @@
-from origin_api import app
-import nltk
-nltk.data.path = ['origin_api/nltk_data']
-
-app=app
--- server/News_Origin/.serverless/newsOrigin-aossie/pdfminer/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = '20200517'
-
-if __name__ == '__main__':
-    print(__version__)
