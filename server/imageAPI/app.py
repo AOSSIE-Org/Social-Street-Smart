80601657e5a6fd3f27597f156772ab3a3c615ffe
Pranav Goyanka <pgoyanka@gmail.com>
2020-08-06 02:56:04 +0530

    Add unit tests for DII API

--- server/imageAPI/app.py
+++ server/imageAPI/app.py
@@ -14,6 +14,7 @@ import bs4
 import requests
 
 app = Flask(__name__)
+app.config['TESTING'] = True
 app.debug=True
 
 headers = {'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Mobile Safari/537.36'}
@@ -30,22 +31,32 @@ def lookup():
         # Then set them as an environment variable
         enc_url = request.args.get('link')
         creds = request.args.get('creds')
-        creds = base64.b64decode(creds)
-        creds = creds.decode("utf-8") 
-        # print(len(creds), file = sys.stderr)
-        if (creds == "undefined"):
+        try:
+            creds = base64.b64decode(creds)
+            creds = creds.decode("utf-8") 
             os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./GoogleAppCreds.json"
-        else:
-            creds = json.loads(creds)
-            with open('/tmp/GoogleAppCreds.json', 'w') as outfile:
-                json.dump(creds, outfile)
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/GoogleAppCreds.json"
+        except:
+            # pass
+        # print(len(creds), file = sys.stderr)
+        # if (creds == "undefined"):
+        #     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./GoogleAppCreds.json"
+        # else:
+            try: 
+                print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+            except:
+                creds = json.loads(creds)
+                with open('/tmp/GoogleAppCreds.json', 'w') as outfile:
+                    json.dump(creds, outfile)
+                os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/GoogleAppCreds.json"
 
         url = request.args.get('link')
         res = imgL.report(imgL.annotate(str(url)))
         print("Result", file = sys.stderr)
         print(jsonify(res), file = sys.stderr)
-        os.remove("/tmp/GoogleAppCreds.json")
+        try:
+            os.remove("/tmp/GoogleAppCreds.json")
+        except: 
+            pass
         return jsonify(res), 200
 
 if __name__ == '__main__':
