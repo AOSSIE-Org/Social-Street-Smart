4951ce8ecfe566a92140719ae5d98f9790b2c1d5
harish2773 <harishd2773@gmail.com>
2023-06-10 10:59:46 +0530

    updated-gulp-to-v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,64 @@
 var gulp = require('gulp'),
   del = require('del'),
   rename = require('gulp-rename'),
-  runSequence = require('run-sequence'),
   htmlmin = require('gulp-htmlmin'),
   minify = require('gulp-minify'),
   csso = require('gulp-csso'),
   eslint = require('gulp-eslint'),
-  gulpIf = require('gulp-if')
-  ;
+  gulpIf = require('gulp-if');
 
-gulp.task('default', function() {
+function defaultTask(done) {
   console.log('Please use the following gulp tasks: clean, build');
-});
+  done();
+}
 
-gulp.task('clean', function() {
+function clean() {
   return del('./dist', {
     force: true
   });
-});
+}
 
-gulp.task('pages', function() {
+function pages() {
   return gulp.src(['./lib/*.html'])
     .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true
     }))
     .pipe(gulp.dest('./dist'));
-});
+}
 
-gulp.task('styles', function () {
+function styles() {
   return gulp.src('./lib/styles/*.css')
     .pipe(csso())
-    .pipe(gulp.dest('./dist/styles'))
-});
+    .pipe(gulp.dest('./dist/styles'));
+}
 
-gulp.task('esLint',()=>{
-  gulp.src(['./lib/assets/js/*.js','./lib/scripts/**/*.js'])
+function esLint() {
+  return gulp.src(['./lib/assets/js/*.js','./lib/scripts/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
     .on('error', function(err) {
-      console.log("Run 'gulp-fix' in terminal to fix these errors"); 
-      process.exit()
+      console.log('Run \'gulp-fix\' in terminal to fix these errors'); 
+      process.exit(1);
     });
-})
-
-function isFixed(file) {
-  return file.eslint != null && file.eslint.fixed;
 }
 
-gulp.task('fix', function () {
+function isFixed(file) {
+  return file.eslint !== null && file.eslint.fixed;
+}
+
+function fix() {
   return gulp.src(['./lib/assets/js/*.js','./lib/scripts/**/*.js'])
     .pipe(eslint({fix:true}))
     .pipe(eslint.format())
     .pipe(gulpIf(isFixed, gulp.dest(function (file) {
-        return file.base;
+      return file.base;
     })))
     .pipe(eslint.failAfterError());
-});
+}
 
-gulp.task('minify', function () {
+function minifyFiles() {
   return gulp.src('./lib/*.js')
     .pipe(minify({
       ext: {
@@ -68,14 +67,22 @@ gulp.task('minify', function () {
       noSource: true,
     }))
     .pipe(gulp.dest('./dist'));
-});
+}
 
-gulp.task('copy-dist', function () {
+function copyDist() {
   gulp.src('./lib/_locales/**/*').pipe(gulp.dest('./dist/_locales/'));
   gulp.src('./lib/assets/**/*').pipe(gulp.dest('./dist/assets/'));
   return gulp.src('./lib/manifest.json').pipe(gulp.dest('./dist/'));
-});
+}
 
-gulp.task('build', function() {
-    runSequence('clean', 'pages', 'styles', 'esLint', 'minify', 'copy-dist');
-});
+const build = gulp.series(clean, gulp.parallel(pages, styles, esLint, minifyFiles, copyDist));
+
+exports.default = defaultTask;
+exports.clean = clean;
+exports.pages = pages;
+exports.styles = styles;
+exports.esLint = esLint;
+exports.fix = fix;
+exports.minifyFiles = minifyFiles;
+exports.copyDist = copyDist;
+exports.build = build;
