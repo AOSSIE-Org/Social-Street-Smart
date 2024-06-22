bac41a9a65ebe5d82caa56bd69c7ed6769e551a1
Vishav Singla <vishav9933@gmail.com>
2024-06-22 18:35:21 +0530

    Click bait API: updated and upgraded old packages and code, its working now (#14)
    click bait api: updated and upgraded old packages and code, its working now
    
    Image API: updated app.py, node.js and deployment files to run all routes in Image API Flask server
    
    Revert "Image API: updated app.py, node.js and deployment files to run all routes in Image API Flask server"
    
    This reverts commit 86838aa3458edb1f3a8a98f4b41161302784d107.

--- server/Click-Bait/clickbait_api/routes.py
+++ server/Click-Bait/clickbait_api/routes.py
@@ -1,93 +1,67 @@
 import pickle
 from tensorflow.keras.models import model_from_json
-import sys, os, re, csv, codecs
+import os
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from flask import request, jsonify, json
-from tensorflow.keras import backend as K
-from clickbait_api import app
+from flask import Flask, request, jsonify
 import tensorflow as tf
 
-from tensorflow.python.keras.backend import set_session
-from tensorflow.python.keras.models import load_model
+app = Flask(__name__)
 
-#tf_config = some_custom_config
-session = tf.Session()
-
-
-#data = pd.read_csv("clickbait_api/resources/final_new.csv")
-
-#sentences = data["titles"].fillna("DUMMY_VALUE").values
-#print(type(sentences))
-import pickle
+# Set up TensorFlow session and graph
+session = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph())
+# Load the tokenizer
 with open('clickbait_api/resources/sentences.pickle', 'rb') as f:
-	sentences= pickle.load(f)
+    sentences = pickle.load(f)
 
-#with open('sentences.pickle', 'wb') as f:
-#    pickle.dump(sentences, f)
-    
-tokenizer = Tokenizer(num_words=22000)
-tokenizer.fit_on_texts(sentences)
+text = 'how are you bro wtf bro'
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(text)
+graph = tf.compat.v1.get_default_graph()
 
-graph = tf.get_default_graph()
+def load_model_func():
+    global loaded_model
+    json_file = open('clickbait_api/resources/lstm_clickbait_new.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("clickbait_api/resources/lstm_clickBait_new.weights.h5")
 
-
-def load_model():
-	global loaded_model
-	json_file = open('clickbait_api/resources/lstm_clickbait_new.json', 'r')
-	loaded_model = json_file.read()
-	json_file.close()
-	loaded_model = model_from_json(loaded_model)
-	loaded_model.load_weights("clickbait_api/resources/lstm_clickBait_new.h5")
-
-set_session(session)
-load_model()
+load_model_func()
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/pred', methods=['GET','POST'])
+@app.route('/pred', methods=['GET', 'POST'])
 def predict():
-	try:
-		if request.method == 'POST':
-			n_str= request.form['text']
+    try:
+        if request.method == 'POST':
+            n_str = request.form['text']
+        elif request.method == 'GET':
+            n_str = request.args.get('text')
 
-		if request.method == 'GET':
-			n_str= request.args.get('text')
-		
-		n_str = n_str.encode('utf-8')
-		#n_str=str(n_str)
-		kk=score(n_str)
-		kk=str(kk)
-		#respo= {'Result': 'none'}
-		#respo['Result'] = str(kk)
-		#print (jsonify(respo)).json()
-		#app.logger.info('API called for string: ' + n_str +'. (returned): ' + kk)
-		return jsonify({'Result': kk}), 200
+        if n_str:
+            n_str = n_str.encode('utf-8')
+            prediction = score(n_str)
+            return jsonify({'Result': str(prediction)}), 200
+        else:
+            return jsonify({'error': 'No text provided'}), 400
 
-
-	except AssertionError as error:
-		pass
-		#app.logger.error('API called for string: ' + n_str.decode("utf-8") + 'Error: '+ error)
-
-
+    except AssertionError as error:
+        return jsonify({'error': str(error)}), 500
 
 def score(n_str):
-	n_str = n_str.decode("utf-8")
-	n_str=[n_str]
-	print (n_str)
-	new_string = tokenizer.texts_to_sequences(n_str)
-	new_string = pad_sequences(new_string, maxlen=200)
-	with session.as_default():
-		with graph.as_default():
-			prediction=loaded_model.predict(new_string)
+    n_str = n_str.decode("utf-8")
+    n_str = [n_str]
+    new_string = tokenizer.texts_to_sequences(n_str)
+    new_string = pad_sequences(new_string, maxlen=200)
+    
+    with session.as_default():
+        with graph.as_default():
+            prediction = loaded_model.predict(new_string)
+    
+    return prediction[0][0]
 
-	#K.clear_session()
-	#prediction = loaded_model.predict(new_string)
-	#print prediction
-	return prediction[0][0]
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
