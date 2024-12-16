c5990c2bac0c665510d2389b05f1c6c2cbb19e4f
Yashwanth M S <yashwanthsince2004@gmail.com>
2024-12-16 18:38:41 +0530

    Changed from openAI to Open Source(Groq)

--- server/Summarizer/summarizer_api/routes.py
+++ server/Summarizer/summarizer_api/routes.py
@@ -2,54 +2,46 @@ from summarizer_api import app
 from newspaper import Article
 from flask import request, jsonify, json
 from urllib.parse import unquote
+from flask import Flask
+from groq import Groq
 
-from openai import OpenAI
+
+app = Flask(__name__)
 
 def summarize(text, ratio):
 	summaryLength = int(len(text)*ratio)
-	client = OpenAI(api_key = "sk-quXV-Cuxe-_yPloGspA1D3a6Xx7lqFKF9xFq4H-O4ET3BlbkFJGFDZJIMDVOOtSF-Itczi_FsI2pdxl97vo-DB3TXJoA")
-
+	client = Groq(api_key="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")
 	completion = client.chat.completions.create(
-		model="gpt-4o-mini",
+		model="llama3-70b-8192",
 		messages=[
 			{"role": "system", "content": "You are a helpful assistant."},
 			{
 				"role": "user",
-				"content": f"Write a summer of the following text:\n{text}\nThe summary should be in {summaryLength} words"
+				"content": f"Write a summary of the following text:\n{text}\nThe summary should be in {summaryLength} words"
 			}
 		],
 		max_tokens=100
 	)
 	return completion.choices[0].message.content
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/pred', methods=['GET','POST'])
+@app.route('/pred', methods=['GET', 'POST'])
 def predict():
-	try:
-		if request.method == 'POST':
-			n_str= request.form['text']
+    try:
+        if request.method == 'POST':
+            n_str = request.form['text']
+        elif request.method == 'GET':
+            n_str = request.args.get('text')
 
-		if request.method == 'GET':
-			n_str= request.args.get('text')
-		
-		n_str = str(n_str)
-		n_str=unquote(n_str)
-		article = Article(n_str, language="en")
-		article.download() 
-		article.parse()
-		summ_per = summarize(article.text, ratio = 0.20) 
-		print(summ_per)
-		return jsonify({'Result': summ_per}), 200
+        n_str = str(n_str)
+        n_str = unquote(n_str)
+        article = Article(n_str, language="en")
+        article.download()
+        article.parse()
 
-
-	except AssertionError as error:
-		app.logger.error('API called for string: ' + n_str.decode("utf-8") + 'Error: '+ error)
-
-
-
-
+        # Generate summary
+        summ_per = summarize(article.text, ratio=0.20)
+        return jsonify({'Result': summ_per}), 200
+    except Exception as error:
+        return jsonify({'Error': str(error)}), 400
 
 
